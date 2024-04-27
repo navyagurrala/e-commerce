9f0837bc0abf1747faa076f9c7141a70d89177c7
navyagurrala <navyasrigurrala21@gmail.com>
2024-04-27 21:48:14 +0530

    tsk

---
+++ script.js
@@ -0,0 +1,25 @@
+let b1 = document.getElementsByTagName("button")
+b1[2].addEventListener("click", () => {
+    window.location.assign("./mens.html")
+})
+
+let b2 = document.getElementsByTagName("button")
+b1[1].addEventListener("click", () => {
+    window.location.assign("./womens.html")
+})
+
+let b3 = document.getElementsByTagName("button")
+b3[3].addEventListener("click", () => {
+    window.location.assign("./jwlry.html")
+})
+
+let b4 = document.getElementsByTagName("button")
+b4[4].addEventListener("click", () => {
+    window.location.assign("./electronics.html")
+})
+
+
+let b5 = document.getElementsByTagName("a")
+b5[6].addEventListener("click", () => {
+    window.location.assign("./cart.html")
+})
